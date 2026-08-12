e2f08b0eba5e8f64e3bf21c12f649f3bfe95288e
Nwokike Kelechi <134720139+Emperor-khay@users.noreply.github.com>
2026-08-12 15:40:48 +0100

    Project Setup

---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,6 @@
+<a href="/" class="flex gap-4">
+    <img class="h-16 w-auto" src="{{ asset('images/house-bridge-logo.png') }}" alt="">
+    <span class="text-[40px] text-hb-secondary">
+        House Bridge
+    </span>
+</a>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h1 class="text-[50px]">This is a header</h1>
+    </x-slot>
+
+    <x-slot>
+        <p class="text-[50px]">This is main content</p>
+    </x-slot>
+</x-app-layout>
